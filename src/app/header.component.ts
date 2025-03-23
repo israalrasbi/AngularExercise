82184606b315998386fdc8dfba6681e9ab59c95e
Isra Al Rasbi <imorasbi@gmail.com>
2025-03-23 10:51:07 +0400

    Added @Component decorator

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,2 +1,11 @@
+import { Component } from "@angular/core";
+
+//add Component decorator that will add metadata and configrations
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+  })
 //need to add the keyword export, so other classes can use it
 export class HeaderComponent {}
